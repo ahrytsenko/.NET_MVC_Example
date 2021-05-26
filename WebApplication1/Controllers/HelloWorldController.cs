44e5342efe87cf21ba08e650c41ae551c03f0255
Andrii Grytsenko <Andrii.Grytsenko@grytsenko.WEBSITE>
2021-05-26 22:27:30 +0300

    Initial Commit

---
+++ WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Encodings.Web;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int numTimes=1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");//
+        }
+    }
+}
